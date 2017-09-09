40c3f185bede538c2b4f0bf9d6321eb85b7dcc7c
QuasiStellar <30569097+QuasiStellar@users.noreply.github.com>
2017-09-10 00:04:35 +0400

    ультра апдейт
    всё исправил, сделал нормальный сэндмессадж
    должно работать, проверь
    сейчас сделаю класс юзеров

--- god.py
+++ god.py
@@ -2,14 +2,37 @@ import cor
 import requests
 import json
 
+
 url = 'https://api.telegram.org/bot'+ cor.tok+'/'
 
-class item:
+
+class TelegramObject:
     def __init__(**args):
-        
-class ReplyKeyboardMarkup(item):
-        
-language_keyboard = ReplyKeyboardMarkup(keyboard = [[eng], [rus]], resize_keyboard = false, one_time_keyboard = true, selective = true)
+        pass
+
+
+class ReplyKeyboardMarkup(TelegramObject):
+    def __init__(self, keyboard, *resize_keyboard = false, *one_time_keyboard = false, *selective = false):
+        self.json_ReplyKeyboardMarkup = json.dumps(
+            'keyboard': keyboard,
+            'resize_keyboard': resize_keyboard,
+            'one_time_keyboard': one_time_keyboard,
+            'selective': selective
+            })
+
+class KeyboardButton(TelegramObject):
+    def __init__(self, text, *request_contact = false, *request_location = false):
+        self.json_KeyboardButton = json.dumps(
+            'text': text,
+            'request_contact': request_contact,
+            'request_location': request_location
+            })
+
+
+eng = KeyboardButton(text = 'English')
+rus = KeyboardButton(text = 'Русский')
+language_keyboard = ReplyKeyboardMarkup(keyboard = [[eng.json_KeyboardButton], [rus.json_KeyboardButton]], one_time_keyboard = true, selective = true)
+
 
 f = open("lud.txt",'r')
 line = f.readline().rstrip()
@@ -32,16 +55,16 @@ def getupdate():
 def get_chat_id():
     global chat_id = requests.get(url+'getupdates').json()['result']['from']['id']
 
-def send_message(chat_id,message_text):
-    requests.post(url+'sendMessage?chat_id='+chat_id+'&text='message_text')
+def send_message(chat_id, text, *parse_mode = false, *disable_web_page_preview = false, *disable_notification = false, *reply_to_message_id = false, *reply_markup = false):
+    requests.post(url+'sendMessage?chat_id='+chat_id+'&text='text'+&parse_mode='parse_mode'+&disable_web_page_preview='disable_web_page_preview'+&disable_notification='disable_notification'+&reply_to_message_id='reply_to_message_id'+&reply_markup='reply_markup')
+
+
 
 def main():
-    update = getupdate() #функция возвращает самое первое обновление, которое еще не возвращала
+    update = getupdate()
     if (last_update_id = 1):
         get_chat_id()
-        send_message(chat_id,'Язык')
-
-
+        send_message(chat_id = chat_id, text = 'Язык', reply_markup = language_keyboard.json_KeyboardButton)
 
 
 
