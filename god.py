e10811eb09707c7b46ca821c4aa615e2e3cfaef2
QuasiStellar <30569097+QuasiStellar@users.noreply.github.com>
2017-09-11 18:30:53 +0400

    Update god.py
    By Artur via QSR's account 
    
    DATABASE CREATED

--- god.py
+++ god.py
@@ -34,37 +34,44 @@ rus = KeyboardButton(text = 'Русский')
 language_keyboard = ReplyKeyboardMarkup(keyboard = [[eng.json_KeyboardButton], [rus.json_KeyboardButton]], one_time_keyboard = true, selective = true)
 
 
+f = open("users_data.json",'r')
+users_data = f.json()
+f.close()
+
+
 f = open("lud.txt",'r')
 line = f.readline().rstrip()
 print(line)
 last_update_id = int(line)
 f.close()
 
+
 def getupdate():
     global last_update_id
-    updates = requests.get(url+'getupdates').json()['result']
-    for update in updates:
-        if (update['update_id'] > last_update_id):
-            f = open("lud.txt",'w')
-            last_update_id = update['update_id']
-            print(last_update_id,file=f)
-            f.close()
-            return update
+    try:
+        updates = requests.get(url+'getupdates').json()['result']
+        for update in updates:
+            if (update['update_id'] > last_update_id):
+                f = open("lud.txt",'w')
+                last_update_id = update['update_id']
+                print(last_update_id,file=f)
+                f.close()
+                return update
     return None
 
-def get_chat_id():
-    global chat_id = requests.get(url+'getupdates').json()['result']['from']['id']
+#def get_chat_id():
+    #global chat_id = requests.get(url+'getupdates').json()['result']['from']['id']
 
 def send_message(chat_id, text, *parse_mode = false, *disable_web_page_preview = false, *disable_notification = false, *reply_to_message_id = false, *reply_markup = false):
-    requests.post(url+'sendMessage?chat_id='+chat_id+'&text='text'+&parse_mode='parse_mode'+&disable_web_page_preview='disable_web_page_preview'+&disable_notification='disable_notification'+&reply_to_message_id='reply_to_message_id'+&reply_markup='reply_markup')
+    requests.post(url+'sendMessage?chat_id='+chat_id+'&text='+text+'&parse_mode='+parse_mode+'&disable_web_page_preview='+disable_web_page_preview+'&disable_notification='+disable_notification+'&reply_to_message_id='+reply_to_message_id+'&reply_markup='+reply_markup)
 
 
 
 def main():
     update = getupdate()
-    if (last_update_id == 1):
-        get_chat_id()
-        send_message(chat_id = chat_id, text = 'Язык', reply_markup = language_keyboard.json_KeyboardButton)
+    if !(update['message']['from']['id'] in users_data):
+        users_data[update['message']['from']['id']] = {}
+    
 
 
 
